2edc72bd8fc3600ed1b8fdcaca0a970449c2865d
Ninja-Lombax <godzillakotm80@gmail.com>
2020-12-06 22:40:01 -0600

    Initial commit

---
+++ HangmanGUIJohnG/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HangmanGUIJohnG
+//
+//  Created by John Grasser on 10/14/20.
+//  Copyright © 2020 John Grasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    
+    
+    @IBOutlet var BtnStart: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
